ef092a5c0fc48d5394fed7efb5be57cefce3491f
Sekwah <sekwah41@gmail.com>
2020-06-28 01:33:14 +0100

    Part way through exploring the asm needed for this

--- src/main/java/info/ata4/minecraft/minema/client/util/ScreenshotHelper.java
+++ src/main/java/info/ata4/minecraft/minema/client/util/ScreenshotHelper.java
@@ -1,6 +1,8 @@
 package info.ata4.minecraft.minema.client.util;
 
 import info.ata4.minecraft.minema.Minema;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 
 import java.awt.image.BufferedImage;
 
@@ -12,5 +14,17 @@ public class ScreenshotHelper
 	public static int getType()
 	{
 		return Minema.instance.getConfig().useAlphaScreenshot.get() ? BufferedImage.TYPE_4BYTE_ABGR : BufferedImage.TYPE_INT_RGB;
+	}
+
+	/**
+	 * Replace the call for normal blend functions
+	 */
+	public static void  replaceBlendFunc(int srcFactor, int dstFactor) {
+		if(srcFactor == GL11.GL_SRC_ALPHA && dstFactor == GL11.GL_ONE_MINUS_SRC_ALPHA) {
+
+		}
+		GL11.glBlendFunc(srcFactor, dstFactor);
+		GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA,
+				GL11.GL_ONE_MINUS_DST_ALPHA, GL11.GL_ONE);
 	}
 }
